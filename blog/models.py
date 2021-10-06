d2d49b8f567720a612256f8e0f7ac31334a3854d
jiveshbhatti <85315081+jiveshbhatti@users.noreply.github.com>
2021-10-06 09:41:51 -0700

    adding python project with django

---
+++ blog/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+status = (0, 'Draft'), (1, 'Publish')
+# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    date_created = models.DateTimeField(auto_now_add=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(to=User, on_delete= models.CASCADE)
+    status = models.IntegerField(choices=status, default=0)
+
+    def __str__(self):
+        return self.title
---
+++ blog/url.py
@@ -0,0 +1,10 @@
+from django.urls.resolvers import URLPattern
+from . import views
+from django.urls import path
+
+urlpatterns = [
+    path('<slug:slug>', views.BlogView.as_view(), name='blog_view'),
+    path('about/', views.AboutView.as_view(),name='about_view'),
+    path('', views.PostList.as_view(), name='home')
+]
+
